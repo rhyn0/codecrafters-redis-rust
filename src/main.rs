6250c5270247256acb9bb0587a53f65680648f71
Ryan Ozawa <ryan.ozawa21@gmail.com>
2023-09-15 20:11:43 -0700

    PONG response

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use std::net::TcpListener;
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buf = String::new();
+    stream.read_to_string(&mut buf).unwrap();
+    stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut _stream) => {
+                handle_connection(_stream);
                 println!("accepted new connection");
             }
             Err(e) => {
